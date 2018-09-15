a6fc2e3471dcc615534cd5b1f9baa500731ca663
Robin Davies <robbiedaves@hotmail.com>
2018-09-15 16:31:01 +0100

    commit travis config and error in index.html

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,28 @@
 import {expect} from 'chai';
+import {JSDOM} from 'jsdom';
+import fs from 'fs';
 
 describe('Our first test', () => {
     it('should pass', () => {
         expect(true).to.equal(true);
+    });
+});
+
+describe('index.html', () => {
+    it('should say hello', () => {
+    // it('should say hello', (done) => {  // if async use the done param
+        const index = fs.readFileSync('./src/index.html', "utf-8");
+        // jsdom.env(index, function(err, window) {
+        //     const h1 = window.document.getElementByTagName('h1')[0];
+        //     expect(h1.innerHTML).to.equal("Hello World!");
+        //     window.close();
+        // });
+        // The above code doesn't work as jsdom.env has been depricated
+        // The new version, use the JSDOM constructor as seen below
+        const dom = new JSDOM(index);
+        const h1 = dom.window.document.getElementsByTagName('h1')[0];
+        expect(h1.innerHTML).to.equal("Hello World!");
+        // done(); // if function is async use the done method to tell 'it' the test has finished!
+        dom.window.close;
     })
 })
